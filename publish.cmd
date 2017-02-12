ce7ba2707cedd4208e165ec2d754c5533af2317d
James Hurburgh <onegodoneloveoneway@gmail.com>
2017-02-12 23:04:25 +1000

    Made headers dynamic and added publish script.

---
+++ publish.cmd
@@ -0,0 +1,6 @@
+copy *.html ..\JamesHurburgh.github.io\allreadyreading /Y
+copy .\css\* ..\JamesHurburgh.github.io\allreadyreading\css /Y
+copy .\fonts\* ..\JamesHurburgh.github.io\allreadyreading\fonts /Y
+copy .\js\* ..\JamesHurburgh.github.io\allreadyreading\js /Y
+copy .\js\app\* ..\JamesHurburgh.github.io\allreadyreading\js\app /Y
+copy .\js\lib\* ..\JamesHurburgh.github.io\allreadyreading\js\lib /Y

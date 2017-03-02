b5af57a128bd21f5ca195cd8bfcbec6f26b4b669
James Hurburgh <onegodoneloveoneway@gmail.com>
2017-03-03 01:27:28 +1000

    Created words page for an overview of all the sets

--- www/js/app/wordDao.js
+++ www/js/app/wordDao.js
@@ -1,25 +1,38 @@
 define(["jquery"],
     function($) {
-        getSetNames = function(){
+        return {
+            getSetNames: function() {
 
-        };
+                return $.getJSON("./js/app/data/sets.json", function(data) {
+                    var items = [];
+                    $.each(data, function(key, val) {
+                        items.push(val.setName);
+                    });
+                    return items;
+                });
+            },
 
-        getSet = function(setName){
-            var set = null;
+            getSet: function(setName) {
+                this.getSetNames().then(function(setNames) {
+                    setNames.sets.forEach(function(set) {
+                        if (set.setName === setName) {
+                            return;
+                        }
+                    });
+                });
+            },
 
-            return set;
-        };
+            getWordListNames: function(setName) {
+                var wordListNames = new ["one", "two", "three"]();
 
-        getWordListNames = function(setName){
-            var wordListNames = new []();
+                return wordListNames;
+            },
 
-            return wordListNames;
-        };
+            getWordList: function(setName, wordListName) {
+                var wordList = null;
 
-        getWordList = function(setName, wordListName){
-            var wordList = null;
-
-            return wordList;
+                return wordList;
+            }
         };
     }
 );

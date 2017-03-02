bd26603cc1653f26612b48774fd87186adec8c19
James Hurburgh <onegodoneloveoneway@gmail.com>
2017-03-02 23:03:36 +1000

    Copied words into own files and began wordDAO

---
+++ www/js/app/wordDao.js
@@ -0,0 +1,25 @@
+define(["jquery"],
+    function($) {
+        getSetNames = function(){
+
+        };
+
+        getSet = function(setName){
+            var set = null;
+
+            return set;
+        };
+
+        getWordListNames = function(setName){
+            var wordListNames = new []();
+
+            return wordListNames;
+        };
+
+        getWordList = function(setName, wordListName){
+            var wordList = null;
+
+            return wordList;
+        };
+    }
+);

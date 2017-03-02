c5a6fb353e0dc94d23b6f61375d011cd14270397
James Hurburgh <onegodoneloveoneway@gmail.com>
2017-03-02 17:09:44 +1000

    Added useful statistics to the profile page

--- www/js/profile.js
+++ www/js/profile.js
@@ -28,8 +28,46 @@ requirejs(['jquery', 'app/common', 'app/profiles'],
 
         var words = Object.getOwnPropertyNames(currentProfile.wordContainer);
         
-        $.each(words, function() {
-            $("#wordStatistics").append(this);
+        $.each(words, function(index, word) {
+            var wordStats = currentProfile.wordContainer[word];
+            
+            var wordDetails = $("<div>")
+            // var wordDetails = $("<a href='#' aria-controls='home' role='tab' data-toggle='tab'>")
+                //.attr("class", "nav-link")
+                .append($("<h4>").append(word));
+            
+            var activityTypes = ["Find", "Spell"];
+            activityTypes.forEach(function(activity) {
+                var activityStats = wordStats[activity];
+                if(!activityStats){
+
+                }else{
+                    var activityDetails = $("<div>");
+                    activityDetails.append(
+                        $("<h5>")
+                        .append(activity));
+                        var stats = $("<div class='progress progress-striped'>");
+                        var correctPercent = activityStats.correctCount/(activityStats.correctCount + activityStats.incorrectCount)*100;
+                        var correctBar = $("<div class='progress-bar progress-bar-success'>")
+                        .append("Correct: " + activityStats.correctCount)
+                        .attr("style", "width: "+correctPercent+"%");
+                        
+                        var incorrectPercent = activityStats.incorrectCount/(activityStats.correctCount + activityStats.incorrectCount)*100;
+                        var incorrectBar = $("<div class='progress-bar progress-bar-danger'>")
+                        .append("Incorrect: " + activityStats.incorrectCount)
+                        .attr("style", "width: "+incorrectPercent+"%");
+                        
+                        stats.append(correctBar);
+                        stats.append(incorrectBar);
+                    activityDetails.append(stats);
+
+                    wordDetails.append(activityDetails);
+                }
+
+            }, this);
+            wordDetails.append("<hr>");
+            //<li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
+            $("#wordStatistics").append(wordDetails);
             
         });
 

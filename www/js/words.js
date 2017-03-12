28259b68dea207fda6266dfa304c14ee48ec45af
James Hurburgh <onegodoneloveoneway@gmail.com>
2017-03-12 23:01:25 +1000

    Began vue implementation

--- www/js/words.js
+++ www/js/words.js
@@ -19,11 +19,31 @@ requirejs.config({
 });
 
 // Start the main app logic.
-requirejs(['jquery', 'app/common', "store", "app/languageCodes", "app/wordDao"],
-    function($, common, store, languageCodes, wordDao) {
+requirejs(['jquery', 'vue', 'app/common', "store", "app/languageCodes", "app/wordDao"],
+    function($, Vue, common, store, languageCodes, wordDao) {
+
+        var app = new Vue({
+            el: '#app',
+            data: {
+                setsCount: 0,
+                sets: []
+            }
+        });
+
+        Vue.component('my-component', {
+            template: '<span>{{ message }}</span>',
+            data: {
+                message: 'hello'
+            }
+        });
+
+        app.sets = wordDao.getSetNames();
         wordDao.getSetNames().then(
             function(setNames) {
                 setNames.sets.forEach(function(set) {
+
+                    app.setsCount += 1;
+
                     var setDisplay = $("<div>")
                         .append(
                             $("<h3>")
@@ -31,19 +51,19 @@ requirejs(['jquery', 'app/common', "store", "app/languageCodes", "app/wordDao"],
                             .attr("data-toggle", "collapse")
                             .attr("data-target", "#" + set.setCode)
                         )
-                        .attr("class", "panel panel");
+                        .attr("class", "panel panel-primary");
 
                     var content = $("<div>")
                         .attr("id", set.setCode)
                         .attr("class", "collapse");
 
                     wordDao.getWordListNames(set.setName).then(
-                        function(wordListNames){
+                        function(wordListNames) {
                             content.append(wordListNames[0]);
                             console.log("Word list length:" + wordListNames.length);
-        
+
                         });
-                        
+
                     setDisplay.append(content);
 
                     $("#sets").append(setDisplay);
